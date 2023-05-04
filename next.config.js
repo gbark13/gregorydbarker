7044c5d75ece2c8b10785b3ac03cd06e63633138
Greg Barker <greg.barker@mac.bid>
2023-05-04 14:16:18 -0400

    adding bella and rewrites

--- next.config.js
+++ next.config.js
@@ -3,6 +3,24 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async rewrites() {
+    return {
+      beforeFiles: [
+        // if the host is `app.acme.com`,
+        // this rewrite will be applied
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'bella.daughter.of.gregorydbarker.com',
+            },
+          ],
+          destination: '/bella/src/app/:path*',
+        },
+      ]
+    }
+  }
 }
 
 module.exports = nextConfig
