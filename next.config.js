0a6d00f34164270bf2ea0e643b48df129c366446
Greg Barker <greg.barker@mac.bid>
2023-05-12 16:35:41 -0400

    put things back

--- next.config.js
+++ next.config.js
@@ -4,24 +4,20 @@ const nextConfig = {
     appDir: true,
   },
   async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'bella.daughter.of.gregorydbarker.com',
-          },
-        ],
-        destination: '/bella/:path*',
-      }
-    ];
-  },
-  webpack: (config) => {
-    // Add your app directory to the module search path
-    config.resolve.modules.push(path.resolve(__dirname, 'src'));
-
-    return config;
+    return {
+      beforeFiles: [
+        {
+          source: '/:path((?!_next).*)',
+          has: [
+            {
+              type: 'host',
+              value: 'bella.daughter.of.gregorydbarker.com',
+            },
+          ],
+          destination: '/bella/:path*',
+        },
+      ]
+    }
   },
 }
 
