19b590dad85760b4793612b8a592d51350c08a58
Greg Barker <greg.barker@mac.bid>
2023-05-12 11:26:25 -0400

    adding some debug to see what's happening

--- src/utils/rewrites.js
+++ src/utils/rewrites.js
@@ -6,7 +6,7 @@ export function useRouterRewrites(router) {
 
     if (asPath.startsWith('/')) {
       const [subdomain] = asPath.split('.');
-
+      console.log("here is the rewrite", {subdomain: subdomain, asPath: asPath})
       if (subdomain === 'bella') {
         const path = asPath.replace(/^\/bella/, '');
         router.push({ query }, path);
